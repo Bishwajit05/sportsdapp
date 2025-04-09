6fa6e52904904bf1f5bfd2dfc044f98f56d69555
Tarun chintakunta <tarunchintakunta@gmail.com>
2025-04-09 18:01:48 +0530

    chore: initialize project with Vite, React, and Tailwind CSS setup
    - Added package.json with project dependencies and scripts for development, building, and testing.
    - Created postcss.config.js for PostCSS configuration with Tailwind CSS and Autoprefixer.
    - Added tailwind.config.js for Tailwind CSS configuration.
    - Created vite.config.js for Vite configuration with necessary plugins and aliasing for browser compatibility.

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+      assert: 'assert',
+      http: 'stream-http',
+      https: 'https-browserify',
+      os: 'os-browserify',
+      url: 'url',
+    },
+  },
+  define: {
+    'process.env': {}
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis',
+      },
+    },
+  },
+})
